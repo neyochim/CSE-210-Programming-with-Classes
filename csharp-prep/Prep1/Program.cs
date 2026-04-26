84dfdc9207be1b4bbc34b75d2eaba1f80d139e8e
neyochim <144850597+neyochim@users.noreply.github.com>
2026-04-25 21:20:55 -0600

    Refactor Program to prompt for first and last name, and display formatted output

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string ney_firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string ney_lastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {ney_lastName}, {ney_firstName} {ney_lastName}.");
     }
 }
